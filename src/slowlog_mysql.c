faa3dcd73e1c5211fbdb6bfee0b05a829a279d02
gilbert <wanghan52@126.com>
2021-11-24 18:21:35 +0800

    1

--- src/slowlog_mysql.c
+++ src/slowlog_mysql.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <errmsg.h>
 
-MYSQL * connection = 0;
+MYSQL * db_connection = 0;
 int is_connected = 0;
 
 char * get_env_value_by_name(char * name) {
@@ -18,13 +18,13 @@ char * get_env_value_by_name(char * name) {
 }
 
 int reconnect_to_db() {
-	connection = mysql_init(0);
-	if (connection == 0) {
+	db_connection = mysql_init(0);
+	if (db_connection == 0) {
 		return -1;
 	}
 
-	if (mysql_real_connect(connection , get_env_value_by_name(VENUS_SLOWLOG_DB_HOST_ENV_VAR) , get_env_value_by_name(VENUS_SLOWLOG_DB_USER_NAME_ENV_VAR) , 
-          get_env_value_by_name(VENUS_SLOWLOG_DB_PASS_ENV_VAR) , 0 , atoi(get_env_value_by_name(VENUS_SLOWLOG_DB_PORT_ENV_VAR)) , 0 , 0) == 0) {
+	if (mysql_real_connect(db_connection , get_env_value_by_name(VENUS_SLOWLOG_DB_HOST_ENV_VAR) , get_env_value_by_name(VENUS_SLOWLOG_DB_USER_NAME_ENV_VAR) , 
+          get_env_value_by_name(VENUS_SLOWLOG_DB_PASS_ENV_VAR) , 0 , atoi(get_env_value_by_name(VENUS_SLOWLOG_DB_PORT_ENV_VAR)) , 0 , 0) == 0) {		
     	return -1;
 	}
 
@@ -41,8 +41,8 @@ int init_mysql_connection() {
 	char db_name[VENUS_SLOWLOG_STR_LENGTH] = {0};
 	sprintf(db_name , "use %s" , get_env_value_by_name(VENUS_SLOWLOG_DB_NAME_ENV_VAR));
 
-	if (mysql_query(connection , db_name)) {
-		int err = mysql_errno(connection);
+	if (mysql_query(db_connection , db_name)) {
+		int err = mysql_errno(db_connection);
 		if (err == 2013 || (err >= 1158 && err <= 1161)) {
 			close_mysql_connection();
 		}
@@ -58,8 +58,8 @@ int write_slowlog_into_mysql(slowlogQElement elem) {
 	(void)sprintf(sql , "insert into redis_log(entry_id , duration , time , client_id , command) values (%ld,%ld,%ld,\'%s\',\'%s\')" , 
 		elem.id , elem.duration , elem.time , elem.peerid , elem.command);
 
-	if (mysql_query(connection , sql)) {
-		int err = mysql_errno(connection);
+	if (mysql_query(db_connection , sql)) {
+		int err = mysql_errno(db_connection);
 		if (err == 2013 || (err >= 1158 && err <= 1161)) {
 			close_mysql_connection();
 			reconnect_to_db();
@@ -74,9 +74,9 @@ int write_slowlog_into_mysql(slowlogQElement elem) {
 void close_mysql_connection() {
 	is_connected = 0;
 
-	if (connection != 0) {		
-		(void)mysql_close(connection);
-		connection = 0;
+	if (db_connection != 0) {		
+		(void)mysql_close(db_connection);
+		db_connection = 0;
 	}
 }
 
