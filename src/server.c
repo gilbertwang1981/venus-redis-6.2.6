62999742bbb568544eefeeed4248854035083787
gilbert <wanghan52@126.com>
2021-11-26 10:34:39 +0800

    1

--- src/server.c
+++ src/server.c
@@ -5601,20 +5601,20 @@ void version(void) {
 }
 
 void usage(void) {
-    fprintf(stderr,"Usage: ./redis-server [/path/to/redis.conf] [options] [-]\n");
-    fprintf(stderr,"       ./redis-server - (read config from stdin)\n");
-    fprintf(stderr,"       ./redis-server -v or --version\n");
-    fprintf(stderr,"       ./redis-server -h or --help\n");
-    fprintf(stderr,"       ./redis-server --test-memory <megabytes>\n\n");
+    fprintf(stderr,"Usage: ./venus-redis-server [/path/to/redis.conf] [options] [-]\n");
+    fprintf(stderr,"       ./venus-redis-server - (read config from stdin)\n");
+    fprintf(stderr,"       ./venus-redis-server -v or --version\n");
+    fprintf(stderr,"       ./venus-redis-server -h or --help\n");
+    fprintf(stderr,"       ./venus-redis-server --test-memory <megabytes>\n\n");
     fprintf(stderr,"Examples:\n");
-    fprintf(stderr,"       ./redis-server (run the server with default conf)\n");
-    fprintf(stderr,"       ./redis-server /etc/redis/6379.conf\n");
-    fprintf(stderr,"       ./redis-server --port 7777\n");
-    fprintf(stderr,"       ./redis-server --port 7777 --replicaof 127.0.0.1 8888\n");
-    fprintf(stderr,"       ./redis-server /etc/myredis.conf --loglevel verbose -\n");
-    fprintf(stderr,"       ./redis-server /etc/myredis.conf --loglevel verbose\n\n");
+    fprintf(stderr,"       ./venus-redis-server (run the server with default conf)\n");
+    fprintf(stderr,"       ./venus-redis-server /etc/redis/6379.conf\n");
+    fprintf(stderr,"       ./venus-redis-server --port 7777\n");
+    fprintf(stderr,"       ./venus-redis-server --port 7777 --replicaof 127.0.0.1 8888\n");
+    fprintf(stderr,"       ./venus-redis-server /etc/myredis.conf --loglevel verbose -\n");
+    fprintf(stderr,"       ./venus-redis-server /etc/myredis.conf --loglevel verbose\n\n");
     fprintf(stderr,"Sentinel mode:\n");
-    fprintf(stderr,"       ./redis-server /etc/sentinel.conf --sentinel\n");
+    fprintf(stderr,"       ./venus-redis-server /etc/sentinel.conf --sentinel\n");
     exit(1);
 }
 
@@ -6321,9 +6321,9 @@ int main(int argc, char **argv) {
     int background = server.daemonize && !server.supervised;
     if (background) daemonize();
 
-    serverLog(LL_WARNING, "oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo");
+    serverLog(LL_WARNING, "oO0OoO0OoO0Oo Venus Redis is starting oO0OoO0OoO0Oo");
     serverLog(LL_WARNING,
-        "Redis version=%s, bits=%d, commit=%s, modified=%d, pid=%d, just started",
+        "Venus Redis version=%s, bits=%d, commit=%s, modified=%d, pid=%d, just started",
             REDIS_VERSION,
             (sizeof(long) == 8) ? 64 : 32,
             redisGitSHA1(),
@@ -6422,9 +6422,9 @@ int main(int argc, char **argv) {
 	}
 
 	if (init_mysql_connection() == -1) {
-		serverLog(LL_WARNING , "connect to DB failed. %s" , strerror(errno));
+		serverLog(LL_WARNING , "connect to MYSQL failed. %s" , strerror(errno));
 	} else {
-		serverLog(LL_NOTICE , "connect to DB success.");
+		serverLog(LL_NOTICE , "connect to MYSQL success.");
 	}
 
     aeMain(server.el);
