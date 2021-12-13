a4544c473324c7e29b3528a3ae30d11a7e40caf6
gilbert <wanghan52@126.com>
2021-12-13 17:24:34 +0800

    1

--- src/venus_db_keepalive_thread.c
+++ src/venus_db_keepalive_thread.c
@@ -1,16 +1,13 @@
 #include "server.h"
 #include "venus_db_keepalive_thread.h"
 #include "slowlogger_def.h"
+#include "slowlogger.h"
 #include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
-#include <arpa/inet.h>
 
 pthread_t venus_db_ka_slowlog_thread_id;
 
@@ -27,37 +24,18 @@ void * run_venus_db_keepalive_thread_event_loop(__attribute((unused)) void * arg
 	while (1) {
 		usleep(VENUS_REDIS_MYSQL_KEEPALVE_INTERVAL);
 
-		int sock = socket(AF_INET , SOCK_STREAM , 0);
-		if (-1 == sock) {
-			serverLog(LL_WARNING , "create socket failed. %s" , strerror(errno));
-		
-			return 0;
+		slowlogQElement elem;
+		(void)memset(elem.command , 0x00 , VENUS_SLOWLOG_DB_SQL_LENGTH);
+		(void)strcpy(elem.command , VENUS_SLOWLOG_KEEPALIVE_DB_COMMAND_NAME);
+		elem.duration = 0;
+		elem.id = -1;
+		(void)sprintf(elem.peerid , "%s:%d" , server.venus_redis_server_host , server.port);
+		elem.time = time(0);
+		if (-1 == putq(&elem)) {
+			serverLog(LL_WARNING , "keepalive failed, write to message queue failed.");
+		} else {
+			serverLog(LL_NOTICE , "TX to self [%s - %s:%d] (keepalive) success." , server.venus_redis_cluster_name , server.venus_redis_server_host , server.port);
 		}
-
-		struct sockaddr_in sockaddr;
-		memset(&sockaddr , 0x00 , sizeof(sockaddr));
-		sockaddr.sin_family = AF_INET;
-		sockaddr.sin_port = htons(server.port);
-		inet_pton(AF_INET ,  server.venus_redis_server_host , &sockaddr.sin_addr);
-		if(connect(sock , (struct sockaddr*)&sockaddr , sizeof(sockaddr)) == -1) {
-			serverLog(LL_WARNING , "connect to redis failed. %s" , strerror(errno));
-		
-			close(sock);
-
-			return 0;
-		}
-
-		char command[VENUS_SLOWLOG_STR_LENGTH] = {0};
-		sprintf(command , "venus-db-keepalive\r\n");
-		if(send(sock , command , strlen(command) , 0) == -1) {
-			serverLog(LL_WARNING , "send keepalive to redis failed. %s" , strerror(errno));
-		
-			close(sock);
-
-			return 0;
-		}
-
-		close(sock);
 	}
 
 	return 0;
